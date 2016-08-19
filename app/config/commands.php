4d41af2d0e2964d909574e85c276376a16695cc9
kpicaza <kpicaza.sound@gmail.com>
2016-08-19 21:29:08 +0200

    Doctrine integration

--- app/config/commands.php
+++ app/config/commands.php
@@ -4,6 +4,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Helper\FormatterHelper;
+use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 
 // Console enviroment default "test"
 $console->getDefinition()->addOption(
@@ -14,5 +16,28 @@ $console->setHelperSet(
     new HelperSet([
         'question' => new QuestionHelper(),
         'formatter' => new FormatterHelper(),
+        'db' => new ConnectionHelper($app["db"]),
+        'em' => new EntityManagerHelper($app["orm.em"]),
     ])
 );
+
+$console->addCommands([
+    new \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand,
+    new \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand,
+    new \Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand,
+    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand,
+    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand,
+    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand,
+    new \Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand,
+    new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand,
+    new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand,
+    new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand,
+    new \Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand,
+    new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand,
+    new \Doctrine\ORM\Tools\Console\Command\InfoCommand,
+    new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand,
+    new \Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand,
+    new \Doctrine\DBAL\Tools\Console\Command\ImportCommand,
+    new \Doctrine\DBAL\Tools\Console\Command\ReservedWordsCommand,
+    new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand
+]);
--- app/config/providers.php
+++ app/config/providers.php
@@ -5,4 +5,7 @@ $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new Silex\Provider\MonologServiceProvider(), $config['monolog']);
 $app->register(new Silex\Provider\FormServiceProvider(), $config['translator']);
 $app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\DoctrineServiceProvider(), $config['doctrine']['dbal']);
+
 $app->register(new \Knp\Provider\ConsoleServiceProvider(), $config['console']);
+$app->register(new \Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider(), $config['doctrine']['orm']);
