7bd7e7ec61bd7b3477694ce8aacdee899f84babb
kpicaza <kpicaza.sound@gmail.com>
2016-10-15 20:42:46 +0200

    Create Range Object

---
+++ packages/range/src/InFw/Range/Range.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace InFw\Range;
+
+class Range
+{
+    /**
+     * @var int
+     */
+    private $min;
+
+    /**
+     * @var int
+     */
+    private $max;
+
+    /**
+     * Range constructor.
+     * @param int $min
+     * @param int $max
+     */
+    public function __construct($min, $max)
+    {
+        if (
+            2 !== count(array_filter([$min, $max], function ($value) {
+                return true === is_int($value);
+            }))
+        ) {
+            throw new \InvalidArgumentException(
+                'All parameters at Size object must be integers.'
+            );
+        }
+
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMin()
+    {
+        return $this->min;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMax()
+    {
+        return $this->max;
+    }
+}
